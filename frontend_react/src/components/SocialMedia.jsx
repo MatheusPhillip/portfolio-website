bc113f291a6c4483df426e48c927fb2d7954e0e2
MatheusMiranda <mpssm@cesar.org.br>
2023-12-14 17:39:32 -0300

    feat: added anchor links to social media icons

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import {BsLinkedin, BsGithub} from 'react-icons/bs';
+import {FaHackerrank} from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
         <div>
-            <BsGithub />
+          <a href="https://github.com/MatheusPhillip" target="_blank" rel="noreferrer"><BsGithub /></a>
         </div>
         <div>
-            <BsLinkedin />
+          <a href="https://www.linkedin.com/in/matheus-miranda-83b3b8186/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
+        </div>
+        <div>
+          <a href="https://www.hackerrank.com/profile/matheuspssmiran1" target="_blank" rel="noreferrer"><FaHackerrank /></a>
         </div>
     </div>
   )
